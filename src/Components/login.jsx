21a02623e40cd0139652c19aa70ea494e07eb185
BautistaMalu <89524797+BautistaMalu@users.noreply.github.com>
2022-08-22 09:36:24 -0300

    Start of login and register page
    Creation of files and setup

---
+++ src/Components/login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './login.css';
+function login() {
+  return (
+    <div>Logueate :) </div>
+  )
+}
+
+export default login
---
+++ src/Components/register.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function register() {
+  return (
+    <div>Registrate :)</div>
+  )
+}
+
+export default register
