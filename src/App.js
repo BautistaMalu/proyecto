c69149f98400af3feb2470de05dd4eeafbe6fbd2
BautistaMalu <89524797+BautistaMalu@users.noreply.github.com>
2022-08-22 09:50:13 -0300

    try of links
    trying to put link from home to register and login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
-import {BrowserRouter as router,Switch,Route,Link} from 'react-router-dom';
+import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom';
+import Login from './Components/login';
+import Register from './Components/register';
 
 function App() {
   return (
-    <div className="App">    </div>
+    <div className="App"> 
+      <header className="App-Header">
+        <h3> Home </h3>
+        <Router>
+            <Routes>
+              
+              <Route path="/login" exact>
+                <Login/>
+                  <Link to='/register'>Registrarse</Link>
+              </Route>
+
+              <Route path="/register" exact>
+                <Register/>
+                  <Link to='/login'>Iniciar Sesion</Link>
+              </Route>
+              
+            </Routes>
+          </Router>
+      </header>
+     </div>
   );
 }
 
