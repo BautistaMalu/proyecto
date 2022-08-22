be1ebb40382fe4d159d54e83eb14f205c9cba5c7
BautistaMalu <89524797+BautistaMalu@users.noreply.github.com>
2022-08-22 09:37:06 -0300

    minor change
    change of code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import {BrowserRouter as router,Switch,Route,Link} from 'react-router-dom';
 
- <title>GroupIt</title>
 function App() {
   return (
     <div className="App">    </div>
