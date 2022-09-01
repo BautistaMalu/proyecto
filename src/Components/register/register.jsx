7d3bb56dc61e97425d65be03de7a01bc2ce909c1
BautistaMalu <89524797+BautistaMalu@users.noreply.github.com>
2022-09-01 13:53:19 -0300

    start of register page
    empiezo a diseñar la pagina de registro

--- src/Components/register/register.jsx
+++ src/Components/register/register.jsx
@@ -4,13 +4,57 @@ import {Link} from 'react-router-dom';
 import Logo from '../login/Logo.png';
 import Foto from '../login/Fotoprueba.png';
 function register() {
-  return (
-    <div className='registrate'> 
-        <div className='contenido-registro'>
-          Registrate<br/> 
 
-          <Link to='/'>Iniciar Sesion</Link>
+  const handlesubmit=(event)=>{
+    event.preventDefault();
+  }
+  
+  return (
+
+    <div className='registrate'> 
+
+      <div className="imagen"> <img src={Logo} alt="" /> </div>
+
+      <div className='contenido-registro'>
+
+      <div className='ladoizq'>
+        
+        <h1 className='registro'> Bienvenido de vuelta <br/> </h1>
+
+        <h3 className="ingrese"> Por favor ingrese sus datos <br/> </h3>
+
+        <form onSubmit={handlesubmit}>
+
+            <label for ="us1">Usuario</label>
+
+            <input placeholder='Ingresa su nombre de usuario' type="text" id='us1'></input>
+
+            <label for ="email1">Email</label>
+
+            <input placeholder="Ingresa su email" type="email" id="email1"/>
+
+            <label for="psw1">Contraseña</label>
+
+            <input placeholder="Ingresa su contraseña" type="password" id="psw1"/>
+            
+            <button type="submit" id="ingreso">Ingresar</button>
+
+            <button type='submit' id='google'> Ingresa con google</button>
+
+        </form>
+
+          <div className="footer">
+            <h3 id='Registrolink'>¿Ya tenes cuenta? <Link to='/'>Inicia Sesion</Link></h3>
+          </div>
+
         </div>
+
+        <div className='ladoder'>
+
+        <div className="Foto"> <img src={Foto} alt="" /> </div>
+
+        </div>
+      </div>
     </div>
 
   )
