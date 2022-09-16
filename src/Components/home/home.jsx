037aea989def18eddb2820b0e0ec43b44db98089
BautistaMalu <89524797+BautistaMalu@users.noreply.github.com>
2022-09-16 14:55:31 -0300

    minor changes

--- src/Components/home/home.jsx
+++ src/Components/home/home.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import './home.css';
 import Header from '../header/header';
 import Footer from '../footer/footer';
+import Ideas from './Ideas.png';
 function home() {
   return (
 
@@ -15,7 +16,20 @@ function home() {
         <div className="contenidohome">
 
           <div className="azul">
-            
+
+            <div className="textos">
+
+              <h1 className="planificar">Planifica y ejecuta tus eventos de una forma eficiente y facil</h1>
+
+              <h4 className="brindar">¡Te brindamos la posibilidad de contactarte con proveedores y distintas opciones que facilitaran tu organizacion!</h4>
+
+              <button className="crear">Empieza a crear</button>
+
+            </div>
+            <div className="blanco1">
+              <img src={Ideas} alt="" className="blancoideas" />
+            </div>
+
           </div>
 
         </div>
