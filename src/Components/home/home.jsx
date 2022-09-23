247ee90f3923098590209f00169874ee0d107ca2
BautistaMalu <89524797+BautistaMalu@users.noreply.github.com>
2022-09-23 14:30:19 -0300

    advances
    more advances en la home

--- src/Components/home/home.jsx
+++ src/Components/home/home.jsx
@@ -48,17 +48,27 @@ function home() {
                 <h4 className="herramientas">Tenemos una gran variedad de herramientas que haran con que tu experiencia sea mucho mas agradable </h4>
                
                 <div className="lista">
-                  <h1 className="titulo1">Lista de invitados</h1>
-                  <h4 className="subtitulo1">Te proporcionamos una lista en donde tendras la posibilidad de ver quienes podran estar presentes en tu evento. </h4>
+                  <img src={Lista} alt="" className="foto1" />
+                  <h3 className="titulo1">Lista de invitados</h3>
+                  <h5 className="subtitulo1">Te proporcionamos una lista en donde tendras la posibilidad de ver quienes podran estar presentes en tu evento. </h5>
                 </div>
 
                 <div className="items">
+                <img src={Items} alt="" className="foto2" />
+                  <h3 className="titulo2">Items a traer</h3>
+                  <h5 className="subtitulo2">Usando nuestro sitio web podras visualizar quienes traen cada item a tu evento, desde una bebida hasta un snack. </h5>
                 </div>
                 
                 <div className="invitaciones">
+                <img src={Invitaciones} alt="" className="foto3" />
+                  <h3 className="titulo3">Invitaciones</h3>
+                  <h5 className="subtitulo3">Tendras la posibilidad de agregar personas a tu evento al mismo tiempo de poder ser invitado a los eventos de los demas. </h5>
                 </div>
 
                 <div className="proveedores">
+                <img src={Proveedores} alt="" className="foto4" />
+                  <h3 className="titulo4">Proveedores </h3>
+                  <h5 className="subtitulo4">Adicionalemente te ofrecemos los medios para contratar el servicio ideal para tu evento. </h5>
                 </div>
 
               </div>
